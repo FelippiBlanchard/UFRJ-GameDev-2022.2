244c021002135024d8d48e5fa48c6d2b60da8ed2
Thiago Mendonça <thiagomendonca.2801@gmail.com>
2022-12-21 23:09:58 -0300

    Add delay config

--- UFRJ-GameDev-2022.2/Assets/_Scripts/PlayerFootsteps.cs
+++ UFRJ-GameDev-2022.2/Assets/_Scripts/PlayerFootsteps.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerFootsteps : MonoBehaviour
 {
     [SerializeField] private AudioSource footstepAudio;
+    [SerializeField] private float jogDelay = 0.15f;
+    [SerializeField] private float sprintDelay = 0.1f;
+    [SerializeField] private float crouchDelay = 0.3f;
 
     public void PlayJogAudio()
     {
